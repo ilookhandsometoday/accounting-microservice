b0817db14126f27b95710d8ea2a05ecaf53d34e5
Nikita Denisov <nikitadenisovvalerevich@gmail.com>
2021-10-28 23:23:05 +0500

    Implement periodic fetching of exchange rate

--- main.py
+++ main.py
@@ -9,16 +9,19 @@ from microservice import AbstractAccountingMicroservice
 
 class AccountingMicroservice(AbstractAccountingMicroservice):
 
-    def __init__(self, currency1: str = "USD", currency2: str = "EUR", currency3: str = "RUB"):
+    def __init__(self, currency1: str = "USD", currency2: str = "EUR", currency3: str = "RUB", period: int = 60):
         self.rate_dict = {currency1: 0, currency2: 0, currency3: 0}
+        self.period = period
 
     async def get_exchange_rate_async(self):
         async with aiohttp.ClientSession() as session:
             async with session.get(r'https://www.cbr-xml-daily.ru/daily_json.js') as response:
-                text: str = await response.text()
-                rates = json.loads(text)['Valute']
-                for key in self.rate_dict.keys():
-                    self.rate_dict[key] = rates[key]["Value"]
+                while True:
+                    text: str = await response.text()
+                    rates = json.loads(text)['Valute']
+                    for key in self.rate_dict.keys():
+                        self.rate_dict[key] = rates[key]["Value"]
+                    await asyncio.sleep(self.period)
         #response = requests.get(r'https://www.cbr-xml-daily.ru/daily_json.js')
         #rate_dict = await json.loads(response.text)['Valute']
 
